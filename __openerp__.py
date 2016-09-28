f78a7b370e8d7cabbbb489985c14488387c0fce3
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-28 08:48:36 -0300

    Added __openerp__

--- __openerp__.py
+++ __openerp__.py
@@ -1,18 +1,9 @@
 {
-    'name': 'Elmatica - Sales Customization',
+    'name': 'Elmatica - Partner Customization',
     'category': 'Sales',
     'version': '0.1',
     'depends': ['base','product'],
     'data': [
-	'product_view.xml',
-	'sale_view.xml',
-	#'po10_report.xml',
-	#'po20_report.xml',
-	#'po21_report.xml',
-	#'shipping_label_report.xml',
-	'security/ir.model.access.csv',
-	'security/security.xml',
-	'wizard/wizard_view.xml'
     ],
     'demo': [
     ],
